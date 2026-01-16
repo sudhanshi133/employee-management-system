996bf5583562ff1d8aaf9c55605c0ee43d600abc
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-01-16 12:16:01 +0530

    file structure added

--- src/main/java/com/tekion/employee/models/EmployeeEntity.java
+++ src/main/java/com/tekion/employee/models/EmployeeEntity.java
@@ -1,4 +1,12 @@
 package com.tekion.employee.models;
 
+import java.time.Instant;
+
+@Document(collection = "employees")
 public class EmployeeEntity {
+
+    @Id
+    private String id;
+    private String name;
+    private Instant createdTime;
 }
