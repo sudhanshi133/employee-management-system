112a7907024dd8d0de4d3e5da0b8081f401b7ed9
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-01-16 12:28:48 +0530

    file structure added

--- src/main/java/com/tekion/employee/models/EmployeeEntity.java
+++ src/main/java/com/tekion/employee/models/EmployeeEntity.java
@@ -1,8 +1,17 @@
 package com.tekion.employee.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.Instant;
 
 @Document(collection = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeEntity {
 
     @Id
